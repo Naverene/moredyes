ac1356b3aa413382a8d9434d4314fbdccb406768
Andyrew617 <andrewtlloyd@gmail.com>
2021-12-11 15:32:00 +1300

    Started adding dataGen

--- src/main/java/net/neverandy/moredyes/data/DataGen.java
+++ src/main/java/net/neverandy/moredyes/data/DataGen.java
@@ -6,10 +6,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 import net.neverandy.moredyes.MoreDyes;
+import net.neverandy.moredyes.data.client.ModBlockStateProvider;
 import net.neverandy.moredyes.data.client.ModItemModelProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Mod.EventBusSubscriber(modid = MoreDyes.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public final class DataGen {
+    public static final Logger LOGGER = LogManager.getLogger();
     private DataGen(){}
 
     @SubscribeEvent
@@ -17,7 +21,9 @@ public final class DataGen {
         DataGenerator gen = event.getGenerator();
         ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
 
-        //gen.addProvider(new ModBlockStateProvider(gen, existingFileHelper));
+        LOGGER.info("add provider blocks");
+        gen.addProvider(new ModBlockStateProvider(gen, existingFileHelper));
+        LOGGER.info("add provider items");
         gen.addProvider(new ModItemModelProvider(gen, existingFileHelper));
     }
 }
--- src/main/java/net/neverandy/moredyes/data/client/ModBlockStateProvider.java
+++ src/main/java/net/neverandy/moredyes/data/client/ModBlockStateProvider.java
@@ -3,20 +3,28 @@ package net.neverandy.moredyes.data.client;
 
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.neverandy.moredyes.MoreDyes;
-import net.neverandy.moredyes.block.MDBlock;
+import net.neverandy.moredyes.block.*;
 import net.neverandy.moredyes.reference.ColorStrings;
 import net.neverandy.moredyes.reference.Reference;
+import net.neverandy.moredyes.setup.ModSetup;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static net.neverandy.moredyes.block.MDBlock.*;
 
 public class ModBlockStateProvider extends BlockStateProvider {
 
     private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
-    private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Reference.MOD_ID);
+    public static final Logger LOGGER = LogManager.getLogger();
+
 
     public ModBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
         super(gen, MoreDyes.MODID, exFileHelper);
@@ -24,15 +32,126 @@ public class ModBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        simpleBlock(block());
+        LOGGER.info("Starting for loop");
+        for(int i=0; i<ColorStrings.ALL.length;i++){
+            simpleBlock(blockGlass(i));
+            simpleBlock(blockSand(i));
+            simpleBlock(blockLeaf(i));
+            simpleBlock(blockBrick(i));
+            simpleBlock(blockClay(i));
+            simpleBlock(blockWool(i));
+            simpleBlock(blockCobble(i));
+            simpleBlock(blockStoneBrick(i));
+            simpleBlock(blockStoneBrickCracked(i));
+            simpleBlock(blockStoneBrickCarved(i));
+            simpleBlock(blockStone(i));
+            simpleBlock(blockObsidian(i));
+            simpleBlock(blockLapis(i));
+            simpleBlock(blockGlowstone(i));
+            simpleBlock(blockCoal(i));
+            simpleBlock(blockSoulSand(i));
+            simpleBlock(blockPlank(i));
+            simpleBlock(blockRedstone(i));
+            simpleBlock(blockQuartz(i));
+            simpleBlock(blockGlassFoggy(i));
+            simpleBlock(blockSapling(i));
+            simpleBlock(blockTulip(i));
+            simpleBlock(blockWorkbench(i));
+            LOGGER.info(i);
+        }
 
     }
 
-    private Block block(){
-            for(int x = 0; x < Reference.BLOCKTYPES.length; x++){
-                for (int i = 0; i < ColorStrings.ALL.length; i++){
-                    return BLOCKS.register();
-                }
-            }
+    private Block blockGlass(int arrayIndex){
+            final String color = ColorStrings.ALL[arrayIndex];
+            return BLOCKS.register("glass_"+color,() -> new BlockGlass(color, Reference.BLOCK_INFO_GLASS, true)).get();
+    }
+    private Block blockSand(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("sand_" + color,() -> new BlockFalling(color, Reference.BLOCK_INFO_SAND)).get();
+    }
+    private Block blockLeaf(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("leaf_"+color,() -> new LeafBlock(color, Reference.BLOCK_INFO_LEAVES)).get();
+    }
+    private Block blockBrick(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("brick_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_BRICK)).get();
+    }
+    private Block blockClay(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("clay_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_CLAY)).get();
+    }
+    private Block blockWool(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("wool_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_WOOL)).get();
+    }
+    private Block blockCobble(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("cobble_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_COBBLE)).get();
+    }
+    private Block blockStoneBrick(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("stonebrick_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_STONE_BRICK)).get();
+    }
+    private Block blockStoneBrickCracked(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("stonebrickcracked_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_STONE_BRICK_CRACKED)).get();
+    }
+    private Block blockStoneBrickCarved(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("stonebrickcarved_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_STONE_BRICK_CARVED)).get();
+    }
+    private Block blockStone(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("stone_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_STONE)).get();
+    }
+    private Block blockObsidian(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("obsidian_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_OBSIDIAN)).get();
+    }
+    private Block blockLapis(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("lapis_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_LAPIS)).get();
+    }
+    private Block blockGlowstone(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("glowstone_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_GLOWSTONE)).get();
+    }
+    private Block blockCoal(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("coal_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_COAL)).get();
+    }
+    private Block blockSoulSand(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("soulsand_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_SOULSAND)).get();
+    }
+    private Block blockPlank(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("plank_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_PLANK)).get();
+    }
+    private Block blockRedstone(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("redstone_"+color,() -> new PoweredBlock(color, Reference.BLOCK_INFO_REDSTONE)).get();
+    }
+    private Block blockQuartz(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("quartz_"+color,() -> new BasicBlock(color, Reference.BLOCK_INFO_QUARTZ)).get();
+    }
+    private Block blockGlassFoggy(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("glassfoggy_"+color,() -> new BlockGlass(color, Reference.BLOCK_INFO_GLASS_FOGGY, true)).get();
+    }
+    private Block blockSapling(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("sapling_"+color,() -> new BlockSapling(color, Reference.BLOCK_INFO_SAPLING)).get();
+    }
+    private Block blockTulip(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("tulip_"+color,() -> new FlowerBlock(color, Reference.BLOCK_INFO_TULIP)).get();
+    }
+    private Block blockWorkbench(int arrayIndex){
+        final String color = ColorStrings.ALL[arrayIndex];
+        return BLOCKS.register("workbench_"+color,() -> new WorkbenchBlock(color, Reference.BLOCK_INFO_WORKBENCH)).get();
     }
 }
